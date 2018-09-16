7261b8b1b37f4361d3da5232f5cae7852040650f
vilozio <korotkovkm@gmail.com>
2018-09-16 16:36:38 +0300

    Updated user-app

--- user-app/src/main/java/org/webapp/entity/User.java
+++ user-app/src/main/java/org/webapp/entity/User.java
@@ -1,10 +1,7 @@
 package org.webapp.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,30 +9,30 @@ import javax.persistence.*;
 
 @Entity
 @Data
-//@ToString(exclude = "password")
-//@NoArgsConstructor
-//@Table(name = "User")
+@ToString(exclude = "password")
+@Table(name = "User")
 public class User {
 
-//    public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+    public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    @NonNull
+    @NonNull
     private String username;
-//    @NonNull
-//    @JsonIgnore
+    @NonNull
+    @JsonIgnore
     private String password;
 
-//    public void setPassword(String password) {
-//        this.password = PASSWORD_ENCODER.encode(password);
-//    }
-//
-//    private User() {}
-//
-//    public User(String username, String password) {
-//        this.username = username;
-//        this.password = password;
-//    }
+    @SuppressWarnings("WeakerAccess")
+    public void setPassword(String password) {
+        this.password = PASSWORD_ENCODER.encode(password);
+    }
+
+    protected User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.setPassword(password);
+    }
 }
--- user-app/src/main/java/org/webapp/service/UserServiceImpl.java
+++ user-app/src/main/java/org/webapp/service/UserServiceImpl.java
@@ -2,19 +2,16 @@ package org.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.bootstrap.config.PropertySourceBootstrapProperties;
 import org.springframework.stereotype.Service;
 import org.webapp.entity.User;
 import org.webapp.kafka.producer.Sender;
 import org.webapp.repository.UserRepository;
 
 @Service
-@EnableConfigurationProperties(PropertySourceBootstrapProperties.class)
 public class UserServiceImpl implements UserService {
 
     @Value("${spring.kafka.topic.userCreated}")
-    private static String USER_CREATED_TOPIC;
+    private String USER_CREATED_TOPIC;
 
     private UserRepository userRepository;
     private Sender sender;
